787f3a9a23805b9f999f97505470df9befe2f5cb
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-11-14 21:54:39 +0530

    fixed dirname

--- build/index.js
+++ build/index.js
@@ -11,14 +11,14 @@ const app = express();
 app.get('/api', (req, res) => {
     res.status(200).json({ message: `Server is listening to port: ${SERVER_PORT}` });
 });
-app.use(express.static(path.resolve(__dirname, '.', 'frontend', 'build')));
+app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
 app.use("/graphql", (0, express_graphql_1.graphqlHTTP)({
     schema: schema_1.default,
     graphiql: true
 }));
 if (NODE_ENV === 'production') {
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '.', 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
 else if (NODE_ENV === 'development') {
