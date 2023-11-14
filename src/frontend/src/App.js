c50cca997eefd25c8a3185bf3319ddb3f21e49b1
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-11-14 21:42:07 +0530

    fixed node react integration correctly

--- src/frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./App.css");
-function App() {
-    return (<div className="App">
-      <h1>Test React App</h1>
-    </div>);
-}
-exports.default = App;
