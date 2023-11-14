eef505fb5d297f0799872ff5c4d8ffc598533a62
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-11-14 21:13:33 +0530

    feat: added react to node server

---
+++ src/frontend/src/App.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./App.css");
+function App() {
+    return (<div className="App">
+      <h1>Test React App</h1>
+    </div>);
+}
+exports.default = App;
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,34 @@ import * as express from "express";
 import * as dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import { graphqlHTTP } from "express-graphql";
+import * as path from "path";
 import schema from "./schema/schema";
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, NODE_ENV } = process.env;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: `Server is listening to port: ${SERVER_PORT}` });
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: `Server is listening to port: ${SERVER_PORT}` });
 });
 
+app.use(express.static(path.resolve(__dirname, './frontend/build')));
+
 app.use("/graphql", graphqlHTTP({
-    schema,
-    graphiql: true
+  schema,
+  graphiql: true
 }));
 
+if (NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './frontend/build', 'index.html'));
+  });
+} else if (NODE_ENV === 'development') {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running!' });
+  });
+}
+
 app.listen(SERVER_PORT, () => {
-    console.log(`Server is listening to port: ${SERVER_PORT}`)
+  console.log(`Server is listening to port: ${SERVER_PORT}`)
 })
