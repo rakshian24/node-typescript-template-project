7abd0d695f43ab6af86ad119e9e2fe915e731b96
rsindhiyar <rakshith_sindhiyar@intuit.com>
2023-11-14 23:01:39 +0530

    fixed the tsconfig node

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import * as express from "express";
+import express from "express";
 import * as dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import { graphqlHTTP } from "express-graphql";
 import * as path from "path";
-import schema from "./schema/schema";
+import { fileURLToPath } from 'url';
+import schema from "./schema/schema.ts";
 
 const { SERVER_PORT, NODE_ENV } = process.env;
 
 const app = express();
 
+// Since we are using E6 modules for NodeJS, __dirname will not be present for modules, we need to do some work around as below for making __dirname to work
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.get('/api', (req, res) => {
-  res.status(200).json({ port: SERVER_PORT, env: NODE_ENV, dirName: __dirname, buildPath:  path.resolve(__dirname, 'frontend', 'build')});
+  res.status(200).json({ port: SERVER_PORT, env: NODE_ENV, dirName: __dirname, buildPath: path.resolve(__dirname, 'frontend', 'build') });
 });
 
 app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
